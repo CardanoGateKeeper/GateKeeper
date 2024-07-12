438ac3b0b3c8e76f18d2e47165a6b079719c4a65
Adam Dean <adam@crypto2099.io>
2024-07-12 16:08:49 -0700

    Initial GateKeeper 1.5 commit

---
+++ app/Models/Policy.php
@@ -0,0 +1,42 @@
+<?php
+
+  namespace App\Models;
+
+  use Illuminate\Database\Eloquent\Factories\HasFactory;
+  use Illuminate\Database\Eloquent\Model;
+  use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+  use Laravel\Jetstream\HasProfilePhoto;
+
+  class Policy extends Model
+  {
+
+    use HasFactory, HasProfilePhoto;
+
+    protected $fillable = [
+      'hash',
+      'name',
+      'team_id',
+      'user_id',
+    ];
+
+    /*protected $visible = [
+      'hash',
+      'name',
+      'profile_photo_url',
+    ];*/
+
+    protected $hidden = [
+      'created_at',
+      'updated_at',
+    ];
+
+    protected $appends = [
+      'profile_photo_url',
+    ];
+
+    public function events(): BelongsToMany
+    {
+      return $this->belongsToMany(Event::class);
+    }
+
+  }
